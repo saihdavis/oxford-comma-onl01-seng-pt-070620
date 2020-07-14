459ca714a41b888c3233771259013eca8faddf49
Saida Davis <saihdavis@gmail.com>
2020-07-14 01:58:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
-end
+  if array.length == 1 
+    array[0]
+  elsif array.length == 2 
+    array.join(" and ")
+  elsif array.length == 3 
+    array[0..1].join(", ") + ", " + "and " + array[2]
+  elsif array.length > 3
+    array[0...-1].join(", ") + "," + " and " + array[-1]
+    
+    
+    
+  end
+  end
